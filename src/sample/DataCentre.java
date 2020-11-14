35ba299ada7b159248f1c5ea0f512afebf4730f5
jkwok678 <jkwok678@gmail.com>
2020-11-14 15:32:02 +0000

    Try RXTX library

--- src/sample/DataCentre.java
+++ src/sample/DataCentre.java
@@ -1,73 +1,109 @@
 package sample;
 
-import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
 
 import java.util.ArrayList;
 
-public class DataCentre {
+public class DataCentre implements SerialPortEventListener {
 
     private SerialPort[] comPortList;
     private SerialPort currentPort;
-    private String portName;
+    private String portName = "COM3";
     ArrayList<Train> trainList;
     String lastLocation ="";
     int x =0;
+    private BufferedReader input;
+    /** The output stream to the port */
+    private OutputStream output;
+    /** Milliseconds to block while waiting for port open */
+    private static final int TIME_OUT = 2000;
+    /** Default bits per second for COM port. */
+    private static final int DATA_RATE = 9600;
 
     public DataCentre()
     {
-        portName = "COM3";
-        comPortList = SerialPort.getCommPorts();
-        setPortFromPortName(portName);
-        currentPort.openPort();
-        currentPort.addDataListener(new SerialPortDataListener() {
-            @Override
-            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
-            @Override
-            public void serialEvent(SerialPortEvent event)
+        initialize();
+    }
+
+    public void initialize() {
+        // the next line is for Raspberry Pi and
+        // gets us into the while loop and was suggested here was suggested https://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
+        //System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        //First, Find an instance of serial port as set in PORT_NAMES.
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            System.out.println(currPortId.getName());
+            if (currPortId.getName().equals(portName))
             {
-                if (event.getEventType() == SerialPort.LISTENING_EVENT_DATA_RECEIVED)
-                {
-                    byte[] newData = event.getReceivedData();
-                    String message = new String(newData);
-                    //String[] parts = message.split(",");
-                    //lastLocation =  parts[0];
-                    //String message2 = message.substring(0,22);
-                    //System.out.print(lastLocation);
-                    //if (!lastLocation.equals(message2))
-                    //{
-                    //    System.out.print(message2);
-                    //    lastLocation = message2;
-                    //}
-                    /*if (x<8)
-                    {
-                        lastLocation = lastLocation + message;
-                        x++;
-                    }
-                    else
-                    {
-                        System.out.print(lastLocation);
-                        lastLocation ="";
-                        x=0;
-                    }
-                    */
-
-
-                    //System.out.print("[ ");
-                    System.out.print(message);
-                    x++;
-                    if (x>8)
-                    {
-                        System.out.println();
-                        x = 0;
-                    }
-                    //System.out.print(" ]");
-                }
-
+                portId = currPortId;
+                break;
             }
-        });
+        }
+        if (portId == null) {
+            System.out.println("Could not find COM port.");
+            return;
+        }
 
+        try {
+            // open serial port, and use class name for the appName.
+            currentPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
+
+            // set port parameters
+            currentPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            // open the streams
+            input = new BufferedReader(new InputStreamReader(currentPort.getInputStream()));
+            output = currentPort.getOutputStream();
+
+            // add event listeners
+            currentPort.addEventListener(this);
+            currentPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    /**
+     * This should be called when you stop using the port.
+     * This will prevent port locking on platforms like Linux.
+     */
+    public synchronized void close() {
+        if (currentPort != null) {
+            currentPort.removeEventListener();
+            currentPort.close();
+        }
+    }
+
+    /**
+     * Handle an event on the serial port. Read the data and print it.
+     */
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine=input.readLine();
+                System.out.println(inputLine);
+            } catch (Exception e) {
+                System.err.println(e.toString());
+            }
+        }
+        // Ignore all the other eventTypes, but you should consider the other ones.
     }
 
     public SerialPort[] getComPortList() {
@@ -91,16 +127,5 @@ public class DataCentre {
 
     public void setPortName(String portName) {
         this.portName = portName;
-    }
-
-    public void setPortFromPortName(String portWanted) {
-        for (SerialPort port : comPortList)
-        {
-            if (port.getSystemPortName().equals(portWanted))
-            {
-                currentPort = port;
-            }
-        }
-
     }
 }
